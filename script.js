1124e4cbd17c35b9fc2ec735f07b1c934fe8b3ed
SethBrendle <seth.bbrendle@gmail.com>
2022-08-22 10:23:09 -0400

    created sections and assigned id's with javascript. created divs for each section with javascript. commented out html to be replaced with javascript html creations

--- script.js
+++ script.js
@@ -1,4 +1,57 @@
+// ==> Creating <section> tags, appending to body, assigning id's
+
+function sectionCreator() {
+    for (let i = 0; i < 3; i++) {
+        const section = document.createElement("section");
+        document.body.appendChild(section);
+    } 
+}
+sectionCreator();
+
+const sectionSetIdHtml = document.getElementsByTagName("section")[0];
+sectionSetIdHtml.setAttribute("id", "html");
+
+const sectionSetIdCss = document.getElementsByTagName("section")[1];
+sectionSetIdCss.setAttribute("id", "css");
+
+const sectionSetIdJavascript = document.getElementsByTagName("section")[2];
+sectionSetIdJavascript.setAttribute("id", "javascript");
+
+function divCreateHtml() {
+    const div = document.createElement("div");
+    document.querySelector("section#html").appendChild(div);
+}
+divCreateHtml();
+
+function divCreateCss() {
+    const div = document.createElement("div");
+    document.querySelector("section#css").appendChild(div);
+}
+divCreateCss();
+
+function divCreateJavascript() {
+    const div = document.createElement("div");
+    document.querySelector("section#javascript").appendChild(div);
+}
+divCreateJavascript();
+
+/*function divCreator () {
+    for (let i = 0; i < 3; i++) {
+        const div = document.createElement("div")
+        document.querySelector("section#html").appendChild(div)
+    }
+}*/
+
+/*
+const createSection = document.createElement("section");
+document.body.appendChild(createSection);
+const sectionSetIdJavaScript = document.getElementsByTagName("section")[2];
+sectionSetIdJavaScript.setAttribute("id", "javascript")
+*/
+
+/*
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+*/
